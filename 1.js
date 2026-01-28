af0bb3dd57548d47f3f4f0c08544935db1ae3187
HarshKamboj25 <harshkamboj297@gmail.com>
2026-01-28 15:40:58 +0530

    feature:timer and adding list and deleteing it

--- 1.js
+++ 1.js
@@ -152,3 +152,77 @@
 // first ();
 
 
+// console.log("first line");
+// setTimeout(()=>{console.log("second line")},2000);
+// console.log("second line");
+// console.log("second line");
+// console.log("second line");
+// console.log("second line");
+// console.log("second line");
+// console.log("second line");
+// console.log("second line");
+// console.log("second line");
+
+
+
+// const timerId=setInterval(() => {
+//     console.log("after 3 seconds")
+// }, 1000);
+// console.log(timerId);
+// setTimeout(() => {
+//     clearInterval(timerId)
+    
+// },10*1000)
+
+// var hello=1
+// const timerId=setInterval(()=>{
+//     hello+=1
+//     console.log(hello)
+// },1000)
+
+// console.log(timerId)
+// setTimeout(()=>{
+//     clearInterval(timerId)
+// },10*1000)       
+
+// let count=1
+
+// const timerId=setInterval(()=>{
+//     if(count===10)clearInterval(timerId)
+//     console.log(count)
+//     count+=1
+
+// },1000)
+
+// console.log("before timeout")
+// setTimeout(()=>{
+//     console.log("inside timeout")
+// },0)
+// console.log("after timeout")
+
+const name=document.querySelector("#name")
+const btn=document.querySelector(".btn")
+const list=document.querySelector(".list")
+
+btn.addEventListener("click",()=>{
+
+    if(name.value==="")return
+    const li=document.createElement("li")
+    const dlt=document.createElement("button")
+
+
+    dlt.innerText="Delete"
+    li.innerText=name.value;
+
+
+    dlt.addEventListener("click",()=>{
+        list.removeChild(li)
+    })
+
+
+    list.appendChild(li)
+    li.appendChild(dlt)
+
+    
+    name.value=""
+})
