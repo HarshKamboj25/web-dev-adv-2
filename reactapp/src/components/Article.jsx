2dddbdf819a3456754b802f7ecc05157c1ef87fd
HarshKamboj25 <harshkamboj297@gmail.com>
2026-04-08 15:56:02 +0530

    last lecture

---
+++ reactapp/src/components/Article.jsx
@@ -0,0 +1,24 @@
+import React,{useState} from 'react'
+import ArtStyle from "../css/Article.module.css"
+
+function Article(props) {
+    const[count,setCount]=useState(0)
+    const[age,setAge]=useState(0)
+    const[name,setName]=useState("")
+
+    const handleIncrement=()=>{
+        setCount(count+1)
+        console.log(count)
+    }
+  return (
+    <div>
+      <h2>Article</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque maiores nemo obcaecati a quam nihil nobis enim quod cupiditate, ipsam sunt laborum vel numquam, sint accusantium. Vitae aliquam esse culpa.</p>
+      <h3 style={{backgroundColor:"red"}}>{props.data}</h3>
+      <h2>{count}</h2>
+      <button className={ArtStyle.btn} onClick={handleIncrement}>Increment</button>
+    </div>
+  )
+}
+
+export default Article
