876867a9f5f1bf2ebc234e960e2970dd16521396
HarshKamboj25 <harshkamboj297@gmail.com>
2026-04-23 12:17:32 +0530

    1

--- reactapp/src/component/Contact.jsx
+++ reactapp/src/component/Contact.jsx
@@ -5,25 +5,26 @@ function Contact() {
     const [email,setEmail]=useState("")
     const [message,setMessage]=useState("")
 
-    const handleSubmit=(data)=>{
-        event.preventDefault()
-        console.log(data)
+    const handleSubmit=(e)=>{
+        e.preventDefault()
+
         if(!email.endsWith("krmu.edu.in")){
             alert("Please enter valid email")
             return 
         }
+
         console.log(name)
         console.log(email)
         console.log(message)
     }
+
   return (
     <div>
-      <form onSubmit={()=>handleSubmit("Sample Data")}>
+      <form onSubmit={handleSubmit}>
         <input type="text" placeholder='Enter your name' value={name} onChange={(e)=>setName(e.target.value)}/>
         <input type="email" placeholder='Enter your email' value={email} onChange={(e)=>setEmail(e.target.value)}/>
         <textarea placeholder='Enter your message' value={message} onChange={(e)=>setMessage(e.target.value)}></textarea>
         <button className='bg-cyan-700 text-white' type='submit'>Submit</button>
-
       </form>
     </div>
   )
